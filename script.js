1574ba3b4366f1f01cc65296b066bd74dcf5b9ee
otooledaniel <otuled93@gmail.com>
2023-01-31 12:14:37 +0100

    done

---
+++ script.js
@@ -0,0 +1,76 @@
+// var a;
+
+// function calc(){
+//    try{
+
+//       fetch("https://facebook.com")
+
+//    }catch(e){
+//       console.log(e)
+//    }
+   
+// }
+
+// calc()
+
+// console.log("hello world")
+
+
+// function handleclick(){
+//    alert("Hello World")
+// }
+
+
+// function handleinput(a, b){
+//    alert(a + b)
+// }
+
+
+
+
+/*
+common js event
+1 onclick
+2 onmousehover
+3 onmouseenter
+4 onmouseleave
+5 ondblclick
+6 onkeyup
+7 onkeydown
+8 onchange
+
+*/
+
+
+// selectors in js
+// document.getElementById(test)
+// document.getElementsByClassName("top")
+// document.querySelector(".")
+// document.querySelectorAll
+// document.getElementsByName
+// document.getElementsByTagName
+
+
+
+
+// var a = document.getElementsByClassName("top")
+
+// console.log(a)
+
+
+// var c = document.getElementsByTagName("div")
+// console.log(c)
+
+// var b = document.querySelector(".top")
+// console.log(b)
+
+
+// const handleclick = () => "hello"
+
+// function handleclick(){
+//    return("hello")
+// }
+
+
+document.getElementById("btn").onclick = ()=> alert("hello")
+
